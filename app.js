d626c24e5a4d661d9bf9211d147f041fa97433a4
Laurence Roberts <lsjroberts@outlook.com>
2014-06-27 00:57:02 +0100

    Fixed database path

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.engine('jade', require('jade').__express)
 // datastore
 var db = {};
 db.rivers = new Datastore({
-    filename: 'storage/database/rivers.table',
-    // filename: '/var/www/pimeariver.com/storage/database/rivers.table',
+    // filename: 'storage/database/rivers.table',
+    filename: '/var/www/pimeariver.com/storage/database/rivers.table',
     autoload: true
 });
 
