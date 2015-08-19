b2dcf107378b146e862e4911e532ae9792e42f3e
Laurence Roberts <lsjroberts@outlook.com>
2015-08-19 02:21:42 +0100

    Updated import to stream json files and write processed sql

--- data/import.js
+++ data/import.js
@@ -1,52 +1,100 @@
-var
-  // Config
-  paths = {
-    shapefile: process.cwd() + '/../raw/natural-earth-data/ne_10m_rivers_lake_centerlines.shp',
-    db: process.cwd() + '/storage/db.sqlite'
-  },
+// Config
+var paths =
+      { db: process.cwd() + '/storage/db.sqlite'
+      , sql: process.cwd() + '/processed/'
+      , json: process.cwd() + '/../raw/open-street-map/river/'
+      }
+  , statements =
+      { river: 'INSERT INTO rivers(`id`, `name`, `source`, `mouth`) VALUES (%d, "%s", %d, %d);\n'
+      , coord: 'INSERT INTO coordinates(`id`, `latitude`, `longitude`) VALUES(%d, %d, %d);\n'
+      , riverCoord: 'INSERT INTO river_coordinates(`riverId`, `coordinateId`) VALUES (%d, %d);\n'
+      }
+  ;
 
-  // Dependencies
-  shapefile = require('shapefile'),
-  sqlite3 = require('sqlite3'),
+// Dependencies
+var fs = require('fs')
+  , util = require('util')
+  , path = require('path')
+  , JSONStream = require('JSONStream')
+  , es = require('event-stream')
+  , _ = require('lodash')
+  , sqlite3 = require('sqlite3')
+  , async = require('async')
+  ;
 
-  // Variables
-  db = new sqlite3.Database(paths.db);
+// Variables
+var db = new sqlite3.Database(paths.db)
+  ;
 
+async.waterfall(
+  [ _.partial(fs.readdir, paths.json)
+  , function (files, next) {
+      async.each(files, processFile, next);
+    }
+  ]
+, function (err) {
+    err && console.error(err);
+    console.log('Done');
+  }
+);
 
-importShapefile(path);
+function processFile (filename, next) {
+  // filename = 's_-10_w_-150_n_0_e_-120.json';
+  var region = filename.replace('.json', '')
+    , filePath = path.join(paths.json, filename)
+    , sqlPath = path.join(paths.sql, region + '.sql')
+    , reader
+    , parser
+    , accumulator
+    , nodes = []
+    ;
 
+  reader = fs.createReadStream(filePath);
+  writer = fs.createWriteStream(sqlPath);
 
-function importShapefile(path) {
-  var reader = shapefile.reader(path)
+  writer.write('-- ' + region + '\n');
 
-  reader.readHeader(function(err, header) {
-    if (err) throw err;
-    readNextRecord(reader);
-  });
-}
+  parser = JSONStream.parse('elements.*');
 
-function readNextRecord(reader) {
-  reader.readRecord(function(err, record) {
-    if (err) throw err;
+  accumulator = es.mapSync(function (data) {
+    var lines = '';
 
-    if (record === shapefile.end) {
-      return reader.close();
+    if (data.type === 'way') {
+      lines = util.format.apply(this,
+        [ statements.river
+        , data.id
+        , (data.tags && data.tags.name) ? data.tags.name : ""
+        , data.nodes[0]
+        , data.nodes[data.nodes.length-1]
+        ]
+      );
+
+      lines += _.reduce(data.nodes, function (r, node) {
+        return r + util.format.apply(this,
+          [ statements.riverCoord
+          , data.id
+          , node
+          ]
+        );
+      }, '');
+    } else if (data.type === 'node') {
+      lines = util.format.apply(this,
+        [ statements.coord
+        , data.id
+        , data.lat
+        , data.lon
+        ]
+      );
     }
 
-    createFeature(transformRecord(record));
-
-    setImmediate(readNextRecord);
+    return lines;
   });
-}
 
-function transformRecord(record) {
-  return {
-    name: record.properties.name,
-    type: record.properties.featurecla,
-    scalerank: record.properties.scalerank
-  };
-}
-
-function createFeature(feature) {
-
+  reader
+    .pipe(parser)
+    .pipe(accumulator)
+    .pipe(writer)
+    .on('end', function () {
+      next();
+    });
 }
--- data/schema.sql
+++ data/schema.sql
@@ -1,20 +1,20 @@
 BEGIN TRANSACTION;
-DROP TABLE `rivers`;
-DROP TABLE `river_coordinates`;
-DROP TABLE `coordinates`
+DROP TABLE IF EXISTS `rivers`;
+DROP TABLE IF EXISTS `river_coordinates`;
+DROP TABLE IF EXISTS `coordinates`;
 CREATE TABLE "rivers" (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`name`	VARCHAR(255),
-	`source`	INTEGER,
-	`mouth`	INTEGER
+  `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name`  VARCHAR(255),
+  `source`  INTEGER,
+  `mouth` INTEGER
 );
 CREATE TABLE "river_coordinates" (
-	`riverId`	INTEGER,
-	`coordinateId`	INTEGER
+  `riverId` INTEGER,
+  `coordinateId`  INTEGER
 );
 CREATE TABLE `coordinates` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`latitude`	NUMERIC,
-	`longitude`	NUMERIC
+  `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
+  `latitude`  NUMERIC,
+  `longitude` NUMERIC
 );
 COMMIT;
