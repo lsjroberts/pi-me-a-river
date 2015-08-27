19a12654fe491068dac68a06e850eb5cb3daaf97
Laurence Roberts <lsjroberts@outlook.com>
2015-08-27 21:35:58 +0100

    Updated solr transform paths, counting and timing

--- data/transform-to-solr.js
+++ data/transform-to-solr.js
@@ -1,7 +1,7 @@
 // Config
 var paths =
-      { json: process.cwd() + '/../raw/open-street-map/river/'
-      , transformed: process.cwd() + '/transformed/'
+      { geojson: process.cwd() + '/geojson/'
+      , json: process.cwd() + '/json/'
       }
   ;
 
@@ -16,25 +16,36 @@ var fs = require('fs')
   ;
 
 // Variables
-var isJsonFile;
+var isGeojsonFile
+  , isJsonFile
+  , fileIndex = 0
+  , fileCount = 0
+  , prevTime = Date.now()
+  ;
 
-isJsonFile = (function (filename) {
+isGeojsonFile = function (filename) {
+  return filename.indexOf('.geojson') !== -1;
+};
+
+isJsonFile = function (filename) {
   return filename.indexOf('.json') !== -1;
-});
+};
 
 async.waterfall(
-  [ _.partial(fs.readdir, paths.json)
+  [ _.partial(fs.readdir, paths.geojson)
   , function (files, next) {
-      files = _.filter(files, isJsonFile);
+      files = _.filter(files, isGeojsonFile);
+
+      fileCount = _.size(files);
 
       async.eachLimit(files, 10, processFile, next);
     }
-  , _.partial(fs.readdir, paths.transformed)
+  , _.partial(fs.readdir, paths.json)
   , function (files, next) {
       files = _.filter(files, isJsonFile);
 
       async.each(files, function (file, done) {
-        fs.appendFile(paths.transformed + file, ']', done);
+        fs.appendFile(paths.json + file, ']', done);
       }, next);
     }
   ]
@@ -45,10 +56,9 @@ async.waterfall(
 );
 
 function processFile (filename, next) {
-  // filename = 's_-10_w_-150_n_0_e_-120.json';
-  var region = filename.replace('.json', '')
-    , filePath = path.join(paths.json, filename)
-    , transformedPath = path.join(paths.transformed, filename)
+  var region = filename.replace('.geojson', '')
+    , geojsonPath = path.join(paths.geojson, filename)
+    , jsonPath = path.join(paths.json, region + '.json')
     , reader
     , parser
     , accumulator
@@ -56,18 +66,18 @@ function processFile (filename, next) {
     , sep = ''
     ;
 
-  reader = fs.createReadStream(filePath);
-  writer = fs.createWriteStream(transformedPath);
+  reader = fs.createReadStream(geojsonPath);
+  writer = fs.createWriteStream(jsonPath);
 
   parser = JSONStream.parse('elements.*');
 
   accumulator = es.mapSync(function (data) {
-    var transformed = {}
+    var json = {}
       , result
       ;
 
     if (data.type === 'relation') {
-      transformed = {
+      json = {
         type: data.type,
         id: data.id,
         ways: _.map(data.members, 'ref'),
@@ -75,7 +85,7 @@ function processFile (filename, next) {
         name_en: (data.tags) ? data.tags['name:en'] : ''
       };
     } else if (data.type === 'way') {
-      transformed = {
+      json = {
         type: data.type,
         id: data.id,
         nodes: data.nodes,
@@ -83,10 +93,10 @@ function processFile (filename, next) {
         name_en: (data.tags) ? data.tags['name:en'] : ''
       };
     } else if (data.type === 'node') {
-      transformed = data;
+      json = data;
     }
 
-    result = sep + JSON.stringify(transformed) + '\n';
+    result = sep + JSON.stringify(json) + '\n';
 
     if (!sep) {
       sep = ',';
@@ -102,6 +112,18 @@ function processFile (filename, next) {
     .pipe(accumulator)
     .pipe(writer)
     .on('finish', function () {
+      var now = Date.now()
+        , diff = now - prevTime
+        ;
+
+      prevTime = now;
+
+      console.log(
+        'Processed', ++fileIndex,
+        'of', fileCount,
+        'in', (diff/60).toFixed(2), 'seconds'
+      );
+
       next();
     });
 }
