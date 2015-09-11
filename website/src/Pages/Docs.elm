0e4b3dbed0b8e0de6753219cc5178021b6c4df17
Laurence Roberts <lsjroberts@outlook.com>
2015-09-11 13:42:40 +0100

    Added temp docs page content

--- website/src/Pages/Docs.elm
+++ website/src/Pages/Docs.elm
@@ -3,9 +3,24 @@ module Pages.Docs where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Signal exposing (Address)
+import Markdown
 
 import Model exposing (..)
 
+import Partials
+
 view : Address Action -> Model -> Html
 view address model =
-  section [ ] [ ]
+  section [ ]
+    [ Partials.pageHeader
+    , docs
+    , Partials.pageFooter address model
+    ]
+
+docs : Html
+docs =
+  Markdown.toHtml """
+  ## API Documentation
+
+  You can [read the full API documenation on apiary](http://docs.pimeariver.apiary.io).
+  """
